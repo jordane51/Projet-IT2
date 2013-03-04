013b622c2b54db6d39916dcc3bd35f524dbef0cc
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-03-04 19:55:54 +0100

    Implémentation à tester sur des epsilons transitions

---
+++ complete.py
@@ -0,0 +1,35 @@
+import automaton
+
+
+def complete(aut):
+    """Return the Automate completed"""
+    trash_is_here = 0
+    aut_bis = aut.clone()
+
+    alphabet = aut_bis.get_alphabet()
+    states = aut_bis.get_states()
+
+    for state in states:
+        for letter in alphabet:
+            transition_tmp = aut_bis.delta(letter, [state])
+            if transition_tmp == set():
+                if not trash_is_here:
+                    aut_bis.add_state("Trash")
+                    trash_is_here = 1
+                aut_bis.add_transition((state, letter, "Trash"))
+
+    return aut_bis
+
+
+def main():
+    aut1 = automaton.automaton(
+        epsilons=[],
+        states=[0, 1], initials=[0], finals=[1],
+        transitions=[(0, 'a', 1), (1, 'b', 0)])
+
+    toto = complete(aut1)
+
+    toto.display()
+
+if __name__ == '__main__':
+    main()
