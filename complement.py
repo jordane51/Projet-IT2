5ffb811e9d8624d7cdcc7b3064a63b52b493a623
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-03-05 16:11:30 +0100

    Rajout du module complement.py

---
+++ complement.py
@@ -0,0 +1,28 @@
+import automaton
+
+
+def complement(aut):
+    states = aut.get_states()
+    finals = set()
+
+    for state in states:
+        if not aut.state_is_final(state):
+            finals.add(state)
+
+    return automaton.automaton(aut.get_alphabet(), aut.get_epsilons(), states,
+                               aut.get_initial_states(), finals, aut.get_transitions())
+
+
+def main():
+    aut1 = automaton.automaton(
+        epsilons=[],
+        states=[0, 1], initials=[0], finals=[1],
+        transitions=[(0, 'a', 1), (1, 'b', 0)])
+
+    aut1.display()
+    toto = complement(aut1)
+
+    toto.display()
+
+if __name__ == '__main__':
+    main()
