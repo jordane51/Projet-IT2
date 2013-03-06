0950c00b752e32b1f453e46011a241c7c41aabbc
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-03-06 16:23:12 +0100

    Rajout du module mirror.py, à revoir, problème positioné sûrement sur l'ajout dans la liste des transitions

---
+++ mirror.py
@@ -0,0 +1,31 @@
+import automaton
+
+
+def mirror(aut):
+    """Return the mirror of an automate """
+    transitions = []
+    transitions_aut = aut.get_transitions()
+
+    for transition in transitions_aut:
+        transitions += [(transition[2], transition[1], transition[0])]
+
+    initial_states = aut.get_final_states()
+    final_states = aut.get_initial_states()
+
+    return automaton.automaton(aut.get_epsilons(), aut.get_states(),
+                               initial_states, final_states, transitions)
+
+
+def main():
+    aut1 = automaton.automaton(
+        epsilons=[],
+        states=[0, 1], initials=[0], finals=[1],
+        transitions=[(0, 'a', 1), (1, 'b', 0)])
+
+    aut1.display()
+    toto = mirror(aut1)
+
+    toto.display()
+
+if __name__ == '__main__':
+    main()
