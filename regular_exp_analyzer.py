9218667643503e1e0c0b270ea4d3b5bfb64aba6b
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-03-26 00:10:03 +0100

    Ajout du module regular_exp_analyzer.py, en cours d'implémentation. Il manque la partie des parenthèses étoiles, sinon il est fonctionnel.

---
+++ regular_exp_analyzer.py
@@ -0,0 +1,55 @@
+def regex_analyzer(string):
+    print(string)
+    liste_res = []
+
+    if '+' in string:
+        toto = string.split('+')
+
+    if len(toto) > 1:
+        liste_res = ['+']
+        for x in toto:
+            # N'a qu'un seul element (donc à mettre directement dans la liste
+            # resultat
+            if len(x) == 1:
+                liste_res += [[x]]
+            # A plus d'un élément, donc est un 'produit' de type
+            # abc -> [., [a], [b], [c]]
+            # et/ou bien à une étoile également à gerer..
+            else:
+                # Gestion des etoiles simple (sans parenthèses pour le moment)
+                if '*' in x:
+                    titi = ['.']
+
+                    # Parcours des couples de caractères. a* teste si suivant
+                    # de 'a' est une étoile, si oui l'ajoute avec le bon format
+                    # et incrémente de 2 le curseur.
+                    # Sinon rajoute normalement le caractère courant.
+                    cpt = 0
+                    while cpt < len(x):
+                        if cpt + 1 < len(x) and x[cpt + 1] == '*':
+                            titi += [['*', x[cpt]]]
+                            cpt += 2
+                            continue
+
+                        titi += [[x[cpt]]]
+                        cpt += 1
+
+                    liste_res += [titi]
+                else:
+                    titi = ['.']
+                    for y in x:
+                        titi += [[y]]
+                    liste_res += [titi]
+
+        return liste_res
+    else:
+        return list(string)
+
+
+def main():
+    string = "a+cv*+b*f*"
+    list_res = regex_analyzer(string)
+    print list_res
+
+if __name__ == '__main__':
+    main()
