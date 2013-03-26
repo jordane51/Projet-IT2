7f3d4c954ba76d2f1e19812a62a69a594b81df12
Bruno THIAOLAYEL <bruno.thiaolayel@gmail.com>
2013-03-27 00:19:52 +0100

    Réimplémentation (2e fois) de regular_exp_analyzer, gestion des '+', '.' et des etoiles AVEC parentheses fonctionne. Manque la gestion des étoiles couplé à une lettre

--- regular_exp_analyzer.py
+++ regular_exp_analyzer.py
@@ -1,47 +1,66 @@
-def regex_analyzer_bis(string):
+def regex_analyzer(string):
     if '+' in string:
-        # Récupère l'indice du premier '+' dans la chaine string.
         plus_index = string.index('+')
         chaine_gauche = string[:plus_index]
         chaine_droite = string[plus_index + 1:]
-        return ['+', [regex_analyzer_bis(chaine_gauche)], [regex_analyzer_bis(chaine_droite)]]
 
-    if '(' in string:
-        # TODO enlever les indices des '(' et ')*'
-        index_parg = string.index(')')
-        index_pard = string.index(')')
-        chaine_gauche = string[:index_parg]
-        chaine_droite = string[index_pard:]
+        #print("chaine_gauche")
+        #print(chaine_gauche)
+        #print("chaine_droite")
+        #print(chaine_droite)
+        #print("--------------")
+        if chaine_droite == '':
+            print("ERREUR, + mal placé")
 
-        #Trouver les indices exact de la chaine entre parentheses
-        chaine_entre_par = string[:]
+        if len(chaine_gauche) >= 2:
+            print("OH EA")
+            if '(' not in chaine_gauche:
+                return ['+', regex_analyzer(chaine_gauche), regex_analyzer(chaine_droite)]
+            elif '(' in chaine_gauche:
+                index_parg = string.index('(')
+                index_pard = string.index(')')
+                chaine_entre_par = string[index_parg + 1:index_pard]
+                print("chaine entre par")
+                print(chaine_entre_par)
 
-        list_regex_gauche = regex_analyzer_bis(chaine_gauche)
-        list_regex_droite = regex_analyzer_bis(chaine_droite)
+                if index_pard + 1 > len(string) or string[index_pard + 1] != '*':
+                    print("ERREUR, * absent après parenthèse")
 
-        if list_regex_gauche:
-            if list_regex_droite:
-                return [[list_regex_gauche], ['*', [regex_analyzer_bis(chaine_entre_par)]], [list_regex_droite]]
-            return [[list_regex_gauche], ['*', [regex_analyzer_bis(chaine_entre_par)]]]
-        return ['*', [regex_analyzer_bis(chaine_entre_par)]]
+                # Cas où il n'y a rien après l'étoile
+                if index_pard + 2 >= len(string):
+                    return ['*', regex_analyzer(chaine_entre_par)]
+                else:
+                    # Si à la suite de l'étoile il y a un '+' alors :
+                    if string[index_pard + 2] == '+':
+                        return ['+', ['*', regex_analyzer(chaine_entre_par)], regex_analyzer(string[index_pard + 3:])]
+                    # Cas où c'est forcément une lettre ou une parenthèse
+                    # ouvrante
+                    # elif string[index_pard + 2] == '(':
+                    else:
+                        return ['.', ['*', regex_analyzer(chaine_entre_par)], regex_analyzer(string[index_pard + 2:])]
 
-    if '*' in string:
-        # Récupère l'indice du premier '*' dans la chaine string
-        if len(string) > 2:
-            etoile_index = string.index('*')
-            chaine_gauche = string[:etoile_index - 1]
-            chaine_droite = string[etoile_index:]
-            list_regex_gauche = regex_analyzer_bis(chaine_gauche)
-            list_regex_droite = regex_analyzer_bis(chaine_droite)
+        return ['+', [chaine_gauche], regex_analyzer(chaine_droite)]
 
-            if list_regex_gauche:
-                if list_regex_droite:
-                    return [list_regex_gauche, ['*', string[etoile_index - 1]], list_regex_droite]
-                return[list_regex_gauche, ['*', string[etoile_index - 1]]]
-            return [['*', string[etoile_index - 1]]]
+    # TODO: Manque les étoiles sans parentheses...
+    #if '*' in string:
+    #    # Récupère l'indice du premier '*' dans la chaine string
+    #    if len(string) > 2:
+    #        etoile_index = string.index('*')
+    #        chaine_gauche = string[:etoile_index - 1]
+    #        chaine_droite = string[etoile_index:]
 
-        return [['*', string[0]]]
+    #        list_regex_gauche = regex_analyzer(chaine_gauche)
+    #        list_regex_droite = regex_analyzer(chaine_droite)
 
+    #        if list_regex_gauche:
+    #            if list_regex_droite:
+    #                return [list_regex_gauche, ['*', string[etoile_index - 1]], list_regex_droite]
+    #            return [list_regex_gauche, ['*', [string[etoile_index - 1]]]]
+    #        return ['*', [string[etoile_index - 1]]]
+
+    #    return ['*', [string[0]]]
+
+    # Cas où il n'y a plus de '+' ou de ')(' à gerer.
     list_res = []
     i = 0
     if len(string) >= 2:
@@ -52,16 +71,15 @@ def regex_analyzer_bis(string):
             if i > len(string):
                 break
     else:
+        # Cas où il n'y a qu'un seul caractere dans la chaine.
         list_res += [string]
 
-    if len(list_res) == 1:
-        return list_res[0]
     return list_res
 
 
 def main():
-    string = "a+b"
-    list_res = regex_analyzer_bis(string)
+    string = "a*b"
+    list_res = regex_analyzer(string)
     print(list_res)
 
 if __name__ == '__main__':
