a512ad8b0f52f8eab71784c9eceb69bfc69f4778
Jordane BARREt <jorbarret@me.com>
2013-03-27 11:09:48 +0100

    Modifs mineures

--- expression_vers_automate.py
+++ expression_vers_automate.py
@@ -1,11 +1,16 @@
 import automaton
 
+def expression_vers_automate(E):
+    for elem in E:
+
+
 def main():
     # ( a+b*a )*
-    test1 = ["*", ["+", ["a", [".", ["*","b"], ["a"]]]]]
+    test1 = ['*', ['+', [['a'], ['.', ['*', ['b']], ['a']]]]]
+    expression_vers_automate(test1)
 
     # ( a+b+c )
-    test2 = ["+", ["+",["a", "b"]], ["c"]
+    #test2 = ["+", ["+",["a", "b"]], ["c"]
 
 if __name__ == '__main__':
     main()
